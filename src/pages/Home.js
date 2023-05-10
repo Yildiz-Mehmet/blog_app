441b9e6b5b79946a9d0744367502fd6547b8c61a
yıldız <mesel4461@gmail.com>
2023-05-10 19:32:14 +0300

    as

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase-config";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [postLists, setPostLists] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+  }, []);
+
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return <div className="post">{post.title}</div>;
+      })}
+    </div>
+  );
 };
 
 export default Home;
